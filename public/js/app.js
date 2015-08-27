3cc7ca20ee65e1cbc56b7aa02823abd04d1b033e
mattscilipoti <matt.scilipoti@ga.co>
2015-08-27 13:36:13 -0400

    functions for the win!
    - functions, functions everywhere!
    - Had fun replacing for loop with #forEach and #map.
    - Since I was reorganizing, I re-indented and added semicolons too.

--- public/js/app.js
+++ public/js/app.js
@@ -1,67 +1,80 @@
-var source   = $("#student").html() || "";
-var forms = $("form")
-var students = document.querySelector(".students")
-var template = Handlebars.compile(source);
+$(document).ready(function(){
+  var source   = $("#student").html() || "";
+  var template = Handlebars.compile(source);
+  var $studentsContainer = $("#students");
+
+  populateStudents($studentsContainer, template);
+  addStudentEvents();
+});
+
+function addStudentEvents(){
+  $("body").on("change", "form", function( event ){
+    event.preventDefault();
+    $.post(this.action, $(this).serialize(), function(res){
+      console.log(res);
+    });
+    this.classList.add("completed");
+  });
+
+  $("body").on("click", ".avatar", function(event){
+    var $form = $(this).closest("form")[0];
+    $form.find("input").attr("checked", false);
+    $form.classList.remove("completed");
+    $.post($form.attr("action"), $form.serialize(), function(res){
+      console.log(res);
+    });
+  });
+
+  $("body").on("click", "#magnifyAvatars", function(event){
+    $avatars = $(".avatar");
+    if(this.checked){
+      $avatars.addClass('magnify');
+    } else {
+      $avatars.removeClass('magnify');
+    }
+  });
+}
 
 function getStudents(callback){
   $.getJSON("http://api.wdidc.org/students?callback=?", function( students ){
-    callback(students)
-  })
+    callback(students);
+  });
+}
+
+function populateStudents($studentsContainer, template){
+  getStudents(function(students){
+    updateStatuses(students, function(){
+      studentEls = renderStudents(students, template);
+      $studentsContainer.append(studentEls);
+    });
+  });
+}
+
+function renderStudents(students, template){
+  return students.map(function(student){
+    return renderStudent(student, template);
+  });
+}
+
+function renderStudent(student, template){
+  student[student.status] = "checked='checked'";
+  var html = template(student);
+  var $el = $(html);
+  if( student.status ){
+    $el.addClass("completed");
+  }
+  return $el;
 }
 
 function updateStatuses(students, callback ){
   $.getJSON("http://api.wdidc.org/attendance"+ window.location.pathname +"?callback=?", function(events){
     for( var i = 0; i < events.length; i++ ){
       for( var j = 0; j < students.length; j++ ){
-	if(events[i].githubUserId == students[j].github_user_id){
-	   students[j].status = events[i].status
-      }
+        if(events[i].githubUserId == students[j].github_user_id){
+          students[j].status = events[i].status;
+        }
       }
     }
-    callback()
-  })
+    callback();
+  });
 }
-
-if(students){
-  getStudents(function(students){
-    updateStatuses(students, function(){
-      for( var i = 0; i < students.length; i++ ){
-	var context = students[i]
-	var status = context.status
-	context[status] = "checked='checked'"
-	var html = template(students[i])
-        var form = $(html)
-        if( context.status ){
-	  form.addClass("completed")
-	}
-	$(".students").append( form )
-      }
-    })
-  })
-}
-
-$("body").on("change", "form", function( event ){
-  event.preventDefault()
-  $.post(this.action, $(this).serialize(), function(res){
-    console.log(res)
-  })
-  this.classList.add("completed")
-})
-
-$("body").on("click", ".avatar", function(event){
-  var form = $(this).closest("form")[0]
-  $(form).find("input").attr("checked", false)
-  form.classList.remove("completed")
-  $.post(form.action, $(form).serialize(), function(res){
-    console.log(res)
-  })
-})
-
-$("#magnifyAvatars").on("click", function(event){
-  avatars = $(".avatar");
-  if(this.checked){
-    avatars.addClass('magnify');
-  } else {
-    avatars.removeClass('magnify');
-  }
-});
